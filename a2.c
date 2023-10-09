cd52e768a1545ae035496d1998476a0b3c81314d
Yang Liu <changzzz@hotmail.com>
2023-10-09 12:38:22 +1100

    4

--- a2.c
+++ a2.c
@@ -15,8 +15,8 @@
 #define DISP 1
 
 
-int master_io(MPI_Comm world_comm, MPI_Comm comm);
-int slave_io(MPI_Comm world_comm, MPI_Comm comm);
+int master_io(MPI_Comm world_comm, MPI_Comm comm,MPI_Comm comm2D,int* coord);
+int slave_io(MPI_Comm world_comm, MPI_Comm comm,MPI_Comm comm2D,int* coord);
 void* ProcessFunc(void *pArg);
 
 pthread_mutex_t g_Mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -57,27 +57,22 @@ int main(int argc, char **argv)
     wrap_around[1]=0;
     reorder = 0;
     ierr = 0;
-	ierr = MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm2D);
+	ierr = MPI_Cart_create(comm, ndims, dims, wrap_around, reorder, &comm2D);
 	if(ierr != 0) printf("ERROR[%d] creating CART\n",ierr);
 
-    if (rank<6){
-    MPI_Cart_coords(comm2D, rank, ndims, coord); // coordinated is returned into the coord array
+    // coordinated is returned into the coord array
+    MPI_Cart_coords(comm2D, my_rank, ndims, coord); 
+
 	MPI_Cart_rank(comm2D, coord, &my_cart_rank);
-
-    
-	MPI_Cart_shift( comm2D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi );
-	MPI_Cart_shift( comm2D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi );
-	printf("Global rank: %d.Cart rank: %d.Coord:(%d, %d).Top: %d.Bottom: %d.Left: %d.Right: %d.\n\n",rank, my_cart_rank, coord[0], coord[1], nbr_j_lo, nbr_j_hi, nbr_i_lo, nbr_i_hi);
-    }
-
+ 
 
     // Create base and node
     MPI_Comm new_comm;
     MPI_Comm_split( MPI_COMM_WORLD, rank == size - 1 , 0, &new_comm);
     if (rank == 0) 
-	master_io( MPI_COMM_WORLD, new_comm );
+	master_io( MPI_COMM_WORLD, new_comm, comm2D,coord );
     else
-	slave_io( MPI_COMM_WORLD, new_comm );
+	slave_io( MPI_COMM_WORLD, new_comm, comm2D,coord );
     MPI_Finalize();
 
 
@@ -89,11 +84,10 @@ int main(int argc, char **argv)
 }
 
 /* This is the base */
-int master_io(MPI_Comm master_comm, MPI_Comm comm) 
+int master_io(MPI_Comm master_comm, MPI_Comm comm,MPI_Comm comm2D,int* coord) 
 {
     int i,j, size, numNode,firstmsg;
-    int nbr_i_lo, nbr_i_hi;
-	int nbr_j_lo, nbr_j_hi;
+
     char buf[256],buf2[256];
     int rank;
 
@@ -102,7 +96,7 @@ int master_io(MPI_Comm master_comm, MPI_Comm comm)
     numNode = size - 1;
     printf("start to receive, numNode%d\n",numNode);
 
-
+    //receive data from node
     while(numNode>0){
 
         MPI_Recv( buf, 256, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, master_comm, &status );
@@ -131,59 +125,53 @@ int master_io(MPI_Comm master_comm, MPI_Comm comm)
 		}
     }
     printf("end of receive\n");
-    MPI_Request send_request[4];
-	MPI_Request receive_request[4];
-	MPI_Status send_status[4];
-	MPI_Status receive_status[4];
-
-    int recvValues[4] = {-1, -1, -1, -1};
-    int neighbors[4] = {nbr_j_lo, nbr_j_hi, nbr_i_lo, nbr_i_hi}; 
-
-    for (int i = 0; i < 6; i++) {
-    MPI_Isend(567, 1, MPI_INT, neighbors[i], 0, comm, &send_request[i]);
-    MPI_Irecv(&recvValues[i], 1, MPI_INT, neighbors[i], 0, comm, &receive_request[i]);
-    }
-
-
-
-    // for (j=1; j<=2; j++) {
-	// for (i=1; i<size; i++) {
-    // 		MPI_Recv( buf, 256, MPI_CHAR, i, 0, master_comm, &status );
-    // 		fputs( buf, stdout );
-	// }
-    // }
+    
 
 
     return 0;
 }
 
+
+
 /* This is the node */
-int slave_io(MPI_Comm master_comm, MPI_Comm comm)
+int slave_io(MPI_Comm master_comm, MPI_Comm comm,MPI_Comm comm2D,int* coord)
 {
     char buf[256];
-    int  rank;
-    
-    MPI_Comm_rank(master_comm, &rank);
-    // sprintf(buf, "Hello from slave %d\n", rank);
-    // MPI_Send( buf, strlen(buf) + 1, MPI_CHAR, 0, 0, master_comm ); // master_comm = MPI_COMM_WORLD
-    
-    // sprintf( buf, "Goodbye from slave %d\n", rank );
-    // MPI_Send( buf, strlen(buf) + 1, MPI_CHAR, 0, 0, master_comm ); // master_comm = MPI_COMM_WORLD
-    
+    int  globalRank,node2dRank;
+    int dims[2],coord[2];
+    MPI_Comm_rank(master_comm, &globalRank);
+
+    MPI_Cart_coords(comm2D, globalRank, 2, coord);
+    MPI_Cart_rank(comm2D, coord, &node2dRank);
     
 
+    sprintf( buf, "Hello from slave %d\n", globalRank );
+    printf("Hello from slave comm2D %d\n", node2dRank );
 
-    sprintf( buf, "Hello from slave %d\n", rank );
-	MPI_Send( buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_ORDERED, master_comm );
+	// MPI_Send( buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_ORDERED, master_comm );
 
-	sprintf(buf, "Goodbye from slave %d\n", rank);
-	MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_ORDERED, master_comm);
+	// sprintf(buf, "Goodbye from slave %d\n", rank);
+	// MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_ORDERED, master_comm);
+    //can use sleep(...) or usleep(...) to delay the process of sending unordered output message for a random timing to simulate the processes sends the message in different timing
+	// sprintf(buf, "I'm exiting (%d)\n", rank);
+	// MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_UNORDERED, master_comm);
 	
-        	//can use sleep(...) or usleep(...) to delay the process of sending unordered output message for a random timing to simulate the processes sends the message in different timing
-	sprintf(buf, "I'm exiting (%d)\n", rank);
-	MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_PRINT_UNORDERED, master_comm);
-	
-	sprintf(buf, "Exit notification from %d\n", rank);
+
+    //print cart
+    int nbr_i_lo, nbr_i_hi;
+	int nbr_j_lo, nbr_j_hi;
+    // MPI_Cart_shift( comm2D, SHIFT_ROW, DISP, &nbr_i_lo, &nbr_i_hi );
+	// MPI_Cart_shift( comm2D, SHIFT_COL, DISP, &nbr_j_lo, &nbr_j_hi );
+    // printf("Global rank: %d. Cart rank: %d.",globalRank,node2dRank);
+
+
+
+    // int recvValues[4] = {-1, -1, -1, -1};
+    // int neighbors[4] = {nbr_j_lo, nbr_j_hi, nbr_i_lo, nbr_i_hi}; 
+
+
+
+	sprintf(buf, "Exit notification from %d\n", globalRank);
 	MPI_Send(buf, strlen(buf) + 1, MPI_CHAR, 0, MSG_EXIT, master_comm);
 
     
